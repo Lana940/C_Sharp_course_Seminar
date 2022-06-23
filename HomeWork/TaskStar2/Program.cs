114b22af8a1330aa158ab4f66a05e684bdadabc7
Svetlana Ponomareva <svetlana.andreyevna@gmail.com>
2022-06-24 01:07:42 +0300

    task 2 not done

---
+++ HomeWork/TaskStar2/Program.cs
@@ -0,0 +1,9 @@
+﻿int N = int.Parse(Console.WriteLine());
+double A  = double.Parse(Console.WriteLine());
+
+
+Console.WriteLine(N = 1 / N);
+Console.WriteLine(A = 1 / A);
+
+
+// не разобралась с дробью, как вывести дроби в ответе в консоли
