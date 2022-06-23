5de8d1d01b23e5427f696a6b16a388f830a10e07
Svetlana Ponomareva <svetlana.andreyevna@gmail.com>
2022-06-23 11:14:06 +0300

    1task made

--- 1_Lesson/1_Proj/Program.cs
+++ 1_Lesson/1_Proj/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, all people!");
+﻿// string num str = Console.ReadLine();
+// int num = int.Parse(num str);
+
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+
+if (num2 == num1 * num1)
+{
+    Console.WriteLine("da");
+
+}
+else
+{
+    Console.WriteLine("net");
+}
+
+
