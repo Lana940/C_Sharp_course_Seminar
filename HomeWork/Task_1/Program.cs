b9948bf6b24682e16b67f79efb0999ef446398d7
Svetlana Ponomareva <svetlana.andreyevna@gmail.com>
2022-06-24 03:06:34 +0300

    task 1 resolved

---
+++ HomeWork/Task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿int numberA = new Random().Next(1, 10); //  0 1 2 3..9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+
+int max = numberA;
+
+if(numberA > numberB) max = numberA;
+
+
+if (numberB > numberA) max = numberB;
+
+{
+    Console.WriteLine( max );
+}
