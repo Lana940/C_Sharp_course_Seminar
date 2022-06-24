2b5687efa5f3220e3281fbd5ed76105422a76d87
Svetlana Ponomareva <svetlana.andreyevna@gmail.com>
2022-06-24 03:15:53 +0300

    Task 2 done

---
+++ HomeWork/Task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿int numberA = new Random().Next(1, 10); //  0 1 2 3..9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int numberC = new Random().Next(1, 10);
+Console.WriteLine(numberC);
+
+int max = numberA;
+
+if(numberA > numberB) max = numberA;
+
+     if (numberB > numberA) max = numberB;
+
+         if (numberC > numberB) max = numberC;
+
+{
+    Console.WriteLine( max );
+}
