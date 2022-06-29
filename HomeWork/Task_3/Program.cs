df3e7015cbb6c03313bfa911affefce61b596309
Svetlana Ponomareva <svetlana.andreyevna@gmail.com>
2022-06-29 22:37:32 +0300

    task 3 with additions

--- HomeWork/Task_3/Program.cs
+++ HomeWork/Task_3/Program.cs
@@ -1,4 +1,5 @@
-﻿int numberA = int.Parse(Console.ReadLine());
+﻿//Напишите программу, которая на вход принимает число и выдает является ли оно четным (%2=1)
+int numberA = int.Parse(Console.ReadLine());
 
 if (numberA % 2 == 1)
 {
