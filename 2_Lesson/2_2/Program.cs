e757a2680be51fdf316deb4cf64b2bfe45c5159f
Svetlana Ponomareva <svetlana.andreyevna@gmail.com>
2022-06-30 23:15:46 +0300

    seminar 2

---
+++ 2_Lesson/2_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите, программу, которая выводит случайное число из отрезка [10,99]
+// и показывает наибольшую цифру числа ex 78 -> 8, 12 -> 2
+
+int MaxNumber(int num)
+{
+
+if(num % 10 > num / 10)
+{
+    return num % 10;
+}
+else
+    return num / 10;
+}
+int SecondNum = new Random().Next(10, 100); // верхняя граница не учитывается, потому 100 а не 99,
+MaxNumber(SecondNum);    
+Console.WriteLine(SecondNum);                 // тк по условию 10 и 99 входят в отрезок
+Console.WriteLine(MaxNumber(SecondNum));
---
+++ 2_Lesson/2_3/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число 
+// кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деление. 34, 5 -> не кратно, остаток 4 , 16, 4 -> кратно
+
+using System;
+
+namespace _2_2
+{
+    class Program
+    {
+        static int CheckinValues(in int val1, in int val2)  // in - указатель на тот кусочек памяти, что задейстовван в  int value1 = int.Parse(Console.ReadLine());/ int value2 = int.Parse(Console.ReadLine());,  сейчас не обязателен
+        {
+            return  (val1 % val2);
+        }
+
+        static void Main(string[] args)                     // 
+        {
+            Console.Write("Введите первое число");
+            int value1 = int.Parse(Console.ReadLine());
+            Console.Write("введите второе число");
+            int value2 = int.Parse(Console.ReadLine());
+
+            int val =  CheckinValues(in value1, in value2);   // in value2
+            if (val == 0)  
+            {
+                Console.WriteLine("Числа кратные");
+            }
+            else
+            {
+                Console.WriteLine($"Остаток от деления {val}");
+            }
+
+        }
+            
+
+
+    }
+}
