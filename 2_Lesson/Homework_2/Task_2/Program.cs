c819eb52da9103c28afb79108ececc0fa3e97bf5
Svetlana Ponomareva <svetlana.andreyevna@gmail.com>
2022-07-01 00:49:23 +0300

    hm 1st and 2nd tasks

---
+++ 2_Lesson/Homework_2/Task_2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+void SecondDigitMiss()
+{
+    int num_1 = new Random().Next(100, 1000);
+    Console.WriteLine(num_1);
+
+    Console.WriteLine($"{num_1 / 100} Secong digit miss {num_1 % 10}");
+
+}
+SecondDigitMiss();
