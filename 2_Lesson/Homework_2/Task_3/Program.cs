62d0c6728887ff5c2057d9c5439ba8a29b3ee6aa
Svetlana Ponomareva <svetlana.andreyevna@gmail.com>
2022-07-01 02:08:38 +0300

    hm added

---
+++ 2_Lesson/Homework_2/Task_3/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// То как пыталась сделать я :
+//// void ThirdDigit( int N)
+//{
+//    while(N > 999)
+//   {
+//      Console.WriteLine($"{N / 10} -> {N % 10}");
+//   } 
+//
+//   if(N < 99)
+// {
+//       Console.WriteLine("3rd digit not exist");
+// }
+//  return;
+//}    
+//Console.WriteLine(ThirdDigit(1254));
+
+// как правильно:
+
+void ThirdDigit(int N)
+{
+    Console.Write($"{N} ->");
+    if (N < 99)
+    {
+        Console.WriteLine("No 3rd digit in this number");
+        return;
+    }
+
+    while (N > 999) N = N / 10;
+    Console.WriteLine(N % 10);
+}
+
+ThirdDigit(int.Parse(Console.ReadLine()));
---
+++ 2_Lesson/Homework_2/Task_4/Program.cs
@@ -0,0 +1,32 @@
+﻿// : Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// void WeekEndsFind()
+//{
+//  int num = new Random().Next(1, 8);
+//Console.WriteLine(num);
+//
+//  if (num > 5)
+//{
+//  Console.WriteLine($"{num} -> Ура, Выходной!");
+//
+//  }
+//else
+//{
+//    Console.WriteLine($"{num} - > Дуй на работу!");
+//}
+//
+//}
+//Console.WriteLine(WeekEndsFind());
+
+//решение из семинара 3
+void Weekend(int num)
+{
+    string text = "no";
+    if (num == 7 || num == 6) text = "yes";
+    Console.WriteLine($"{num} -> {text}");
+
+}
+
+Weekend(6);
+Weekend(7);
+Weekend(1);
