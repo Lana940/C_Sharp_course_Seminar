ea0a0459e28ca8e1087c193117c601cde0df6db9
Svetlana Ponomareva <svetlana.andreyevna@gmail.com>
2022-07-03 02:51:56 +0300

    hm 3 seminar

---
+++ 3_Lesson/HomeWork3/HM_Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно
+// палиндромом. 14212 -> нет  23432 -> да
+
+Console.Write("Put your number: ");
+string number = Console.ReadLine();
+
+void SameNumbers(string number)
+{
+  if (number[0] == number[4] || number[1] == number[3])
+  {
+    Console.WriteLine($" The {number} is a palindrome.");
+  }
+  else Console.WriteLine($"The {number} - is not a palindrome.");
+}
+
+if (number.Length == 5)
+{
+  SameNumbers(number);
+}
+else Console.WriteLine($"Put the correct number");
+
+
---
+++ 3_Lesson/HomeWork3/HM_Task2/Program.cs
@@ -0,0 +1,10 @@
+﻿// : Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между
+// ними в 3D пространстве.
+
+double XYDistance(double x1, double x2, double y1, double y2, double z1, double z2)
+{
+    return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2)); //  AB = √(xb - xa)2 + (yb - ya)2 + (zb -za)2
+}
+
+XYDistance(3,6,2,1,4,8);
+Console.WriteLine(XYDistance(3,6,2,1,4,8));
