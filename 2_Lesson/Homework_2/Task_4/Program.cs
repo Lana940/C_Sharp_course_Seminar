707a9652e14cf795f5b6a5db824431666de6c47a
Svetlana Ponomareva <svetlana.andreyevna@gmail.com>
2022-07-02 22:23:31 +0300

    task 4 my and seminar's lesson 2

--- 2_Lesson/Homework_2/Task_4/Program.cs
+++ 2_Lesson/Homework_2/Task_4/Program.cs
@@ -1,24 +1,25 @@
 ﻿// : Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
-// void WeekEndsFind()
-//{
-//  int num = new Random().Next(1, 8);
-//Console.WriteLine(num);
-//
-//  if (num > 5)
-//{
-//  Console.WriteLine($"{num} -> Ура, Выходной!");
-//
-//  }
-//else
-//{
-//    Console.WriteLine($"{num} - > Дуй на работу!");
-//}
-//
-//}
-//Console.WriteLine(WeekEndsFind());
+void WeekEndsFind()
+{
+    int num = new Random().Next(1, 8);
+    Console.WriteLine(num);
 
-//решение из семинара 3
+    if (num > 5)
+    {
+        Console.WriteLine($"{num} -> Ура, Выходной!");
+
+    }
+    else
+    {
+        Console.WriteLine($"{num} - > Дуй на работу!");
+    }
+
+}
+WeekEndsFind();
+
+----------------------------
+////решение из семинара 3
 void Weekend(int num)
 {
     string text = "no";
