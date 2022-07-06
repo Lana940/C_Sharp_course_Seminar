59c2237752cd0169e67f5c2b328083ac16a309cd
Svetlana Ponomareva <svetlana.andreyevna@gmail.com>
2022-07-06 17:40:20 +0300

    notes added

--- 3_Lesson/HomeWork3/HM_Task1/Program.cs
+++ 3_Lesson/HomeWork3/HM_Task1/Program.cs
@@ -1,22 +1,29 @@
 ﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно
 // палиндромом. 14212 -> нет  23432 -> да
-
-Console.Write("Put your number: ");
-string number = Console.ReadLine();
-
-void SameNumbers(string number)
+//Общее решение без ограничения в индексах
+void Palindrome (int n)   //Method
 {
-  if (number[0] == number[4] || number[1] == number[3])
+  int i = 0;
+  string num = n.ToString();
+  int size = num.Length - 1;
+  string flag = "yes";
+
+  while (i < size/2)
   {
-    Console.WriteLine($" The {number} is a palindrome.");
+    if (num[i] != num[size - i]) // !=  - ne ravno, esli 1 i posledniy element ne sovpadaet, srazu zhe
+    {
+      flag = "no";               // vyhodim iz proverki i wywodim no
+      break;
+    }
+    i += 1;
   }
-  else Console.WriteLine($"The {number} - is not a palindrome.");
+  Console.WriteLine($"{n} -> {flag}");  
 }
 
-if (number.Length == 5)
-{
-  SameNumbers(number);
-}
-else Console.WriteLine($"Put the correct number");
+Palindrome(14212);
+Palindrome(23432);
+Palindrome(12821);
+Palindrome(1248421);
+Palindrome(12488421);
 
 
