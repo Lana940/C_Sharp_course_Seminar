2bf190943dc7f69f608c73488fbf0904b7e613b6
Svetlana Ponomareva <svetlana.andreyevna@gmail.com>
2022-07-10 20:53:11 +0300

    hm variant  by teacher

--- 4_Lesson/HM_4/HM_Task2/Program.cs
+++ 4_Lesson/HM_4/HM_Task2/Program.cs
@@ -3,15 +3,31 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
-int NumSum (int num)
-{   int result = 0;
-    while(num > 0)
+int NumSum(int num)
+{
+    int result = 0;
+    while (num > 0)
     {
         result = result + num % 10;
         num = num / 10;
     }
-    return result; 
+    return result;
 }
 Console.WriteLine(NumSum(452));
 Console.WriteLine(NumSum(82));
 Console.WriteLine(NumSum(9012));
+
+
+// вариант с использование for 
+
+// int NumSum(int num)
+// {
+//     int result = 0;
+
+//     for (int i = num; i != 0; i /= 10)
+//     {
+//         result = result + num % 10;
+//     }
+//     return result;
+
+// }
---
+++ 4_Lesson/HM_4/HM_Task3_teachers_version/Program.cs
@@ -0,0 +1,34 @@
+﻿// : Напишите программу, которая задаёт массив из
+// 8 элементов случайными числами и выводит их на экран.
+// Оформите заполнение массива и вывод в виде функции
+// (пригодится в следующих задачах)   1,2,5,7,19,1,2,8 -> 1,2,5,7,19,1,2,8
+
+// Вариант преподавателя:
+
+void Print( int [] arr)
+{
+    int size = arr.Length;
+    for (int i = 0;  i < size; i++)
+    {
+        Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine();
+}
+
+int [] EightMassive()    //   - int [] - возвращает нам массив
+{
+    int size = 8;
+    int [] arr = new int [size];   // - создаем новый целочиц массив из 8 позиций
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(1,100);
+    }
+    return arr;
+}
+
+int [] arr_1 = EightMassive();    // для сохранения массива, поскольку сейчас он только внутри метода
+Print(arr_1);
+int [] arr_2 = EightMassive();
+Print(arr_2); 
+int [] arr_3 = EightMassive();
+Print(arr_3); 
