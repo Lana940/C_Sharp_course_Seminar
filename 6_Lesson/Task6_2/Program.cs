e8e751163643f850b144f9553c8f965f740aa3f9
Svetlana Ponomareva <svetlana.andreyevna@gmail.com>
2022-07-14 22:34:20 +0300

    seminar's tasks

--- 6_Lesson/Task6_2/Program.cs
+++ 6_Lesson/Task6_2/Program.cs
@@ -4,3 +4,15 @@
 // 3 -> 11
 // 2 -> 10
 
+void DecimalToBinary(int num)
+{
+    string result = "";
+    while (num > 0)
+    {
+        result = num % 2 + result; //result += num % 2;
+        num = num / 2;
+    }
+    Console.WriteLine(result);
+
+}
+DecimalToBinary(42);
---
+++ 6_Lesson/Task6_4/Program.cs
@@ -0,0 +1,45 @@
+﻿// : Напишите программу, которая будет создавать
+// копию заданного массива с помощью поэлементного
+// копирования. 
+
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10); 
+        }
+    }
+}
+
+void CopyArray(int [,] matr)
+{
+    Console.WriteLine($"{matr} ");
+}
+Console.WriteLine();
+
+int[,] matrix = new int[2, 4];
+PrintArray(matrix);
+Console.WriteLine();
+
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine();
+
+CopyArray(matrix);
+PrintArray(matrix);
