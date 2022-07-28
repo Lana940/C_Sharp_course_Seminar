06eb3d2fd2cdcb735156a9996d19f4fc58c7c645
Svetlana Ponomareva <svetlana.andreyevna@gmail.com>
2022-07-28 23:37:25 +0300

    hm 1 task

---
+++ 9_Lesson/HM_9/HM_9_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+void EvenSequence(int M, int N)
+{
+    if (N < M) return;
+    if (M % 2 != 0) M++;
+    EvenSequence(M + 2, N);
+    Console.Write($"{M},  ");
+    
+
+}
+EvenSequence(4, 8);
+Console.WriteLine();
+EvenSequence(2, 10);
+Console.WriteLine();
+EvenSequence(1, 5);
